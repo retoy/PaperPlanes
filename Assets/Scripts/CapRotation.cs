f6a9ea57f748db8eadfec57cd5210a928c431447
Monsterkot <sipa6572@gmail.com>
2023-08-10 16:27:36 +0300

    Bug fix

--- Assets/Scripts/CapRotation.cs
+++ Assets/Scripts/CapRotation.cs
@@ -15,10 +15,15 @@ namespace Appegy
         {
             switch (Level.GetPrivateCurve())
             {
-                case 0:
+                case LevelConfig.Curve.Linear:
                     float rotationAmount = Level.GetPrivateAnglePerSec() * Time.deltaTime;
                     transform.Rotate(0, 0, rotationAmount);
                 break;
+
+                case LevelConfig.Curve.Linear2:
+                    rotationAmount = Level.GetPrivateAnglePerSec() * Time.deltaTime;
+                    transform.Rotate(0, 0, rotationAmount + 10000);
+                break;  
             }                        
         }
     }
--- Assets/Scripts/LevelConfig.cs
+++ Assets/Scripts/LevelConfig.cs
@@ -8,7 +8,8 @@ namespace Appegy
     {
         public enum Curve
         {   
-            Linear
+            Linear,
+            Linear2
         }
         [SerializeField]
         private Sprite sprite;
