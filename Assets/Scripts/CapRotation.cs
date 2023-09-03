9248f293cb712a2d9b74dc23e34392ff3d738c7e
Monsterkot <sipa6572@gmail.com>
2023-09-03 17:27:48 +0300

    Easing Func Multiplier added

--- Assets/Scripts/CapRotation.cs
+++ Assets/Scripts/CapRotation.cs
@@ -6,12 +6,13 @@ namespace Appegy
     {
         [SerializeField]
         private LevelConfig _level;
+        [SerializeField]
+        private float _easingFuncMultiplier;
         private float _time;
-        
         private void Update()
         {
             _time += Time.deltaTime;
-            var t = (2f * EasingFunc.InOutSinusoidal(_time) + Mathf.PI + 2f) * 200f;
+            var t = (2f * EasingFunc.InOutSinusoidal(_time) + Mathf.PI + 2f) * _easingFuncMultiplier;
             transform.eulerAngles = new Vector3(0, 0, t);
         }      
     }
